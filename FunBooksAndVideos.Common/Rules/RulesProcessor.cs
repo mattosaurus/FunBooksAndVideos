d6185cc47c89e095f22111903bc08ec0bc57f49b
mattosaurus <mattosaurus@outlook.com>
2019-12-24 11:16:01 +0000

    Add rule processor

--- FunBooksAndVideos.Common/Rules/RulesProcessor.cs
+++ FunBooksAndVideos.Common/Rules/RulesProcessor.cs
@@ -1,10 +1,26 @@
-﻿using System;
+﻿using FunBooksAndVideos.Common.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace FunBooksAndVideos.Common.Rules
 {
-    class RulesProcessor
+    public class RulesProcessor
     {
+        private readonly IEnumerable<IRule> _rules;
+
+        public RulesProcessor(IEnumerable<IRule> rules)
+        {
+            _rules = rules;
+        }
+
+        public async Task ProcessOrderAsync(IPurchaseOrder purchaseOrder)
+        {
+            foreach (IRule rule in _rules)
+            {
+                await rule.ApplyRuleAsync(purchaseOrder);
+            }
+        }
     }
 }
