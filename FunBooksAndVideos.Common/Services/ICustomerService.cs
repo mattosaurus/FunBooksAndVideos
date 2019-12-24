23c68ad1766c549c8da1fbdc1214942105a9b7d5
mattosaurus <mattosaurus@outlook.com>
2019-12-24 10:50:13 +0000

    Add services

---
+++ FunBooksAndVideos.Common/Services/ICustomerService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunBooksAndVideos.Common.Services
+{
+    public interface ICustomerService
+    {
+        Task ActivateMembershipAsync(int customerId, int productId);
+    }
+}
---
+++ FunBooksAndVideos.Common/Services/IShippingService.cs
@@ -0,0 +1,13 @@
+﻿using FunBooksAndVideos.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunBooksAndVideos.Common.Services
+{
+    public interface IShippingService
+    {
+        Task<ShippingSlip> GenerateShippingSlipAsync(int customerId, IEnumerable<PurchaseItem> purchaseItems);
+    }
+}
